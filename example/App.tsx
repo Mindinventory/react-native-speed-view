4112bb52035ac57031fdc41a031702d9be465ece
Tushar Sanchaniya <tushar.sanchaniya@mindinventory.com>
2022-05-11 10:20:56 +0530

    - Change logic for circle size and pass in props. - Improvise logic to make inner circle adjustable as per size that user provide. - Adjust percentage text component to show in center of inner circle.

--- example/App.tsx
+++ example/App.tsx
@@ -5,22 +5,58 @@ import SpeedView from 'react-native-speed-view';
 import { StyleSheet, Text } from 'react-native';
 
 const App = () => {
-  const [progressValue, setProgressValue] = useState<number>(0);
+  const initialvalue = 35;
+  const [progressValue, setProgressValue] = useState<number>(initialvalue);
+  const [circleSize, setCircleSize] = useState<number>(150);
+  
   return (
     <SafeAreaView style={styles.safeAreaStyle}>
-      <View style={styles.sliderContainerStyle}>
-        <Slider
-          style={styles.sliderStyle}
-          maximumValue={100}
-          minimumValue={0}
-          maximumTrackTintColor="lightgray"
-          onSlidingComplete={(val) => {
-            setProgressValue(Math.round(val));
-          }}
-        />
-        <Text style={styles.progressStyle}>{progressValue}%</Text>
+      <View>
+        <Text
+          style={[styles.progressStyle, { alignSelf: 'center', marginTop: 30 }]}
+        >
+          Circle Progress
+        </Text>
+        <View style={styles.sliderContainerStyle}>
+          <Slider
+            style={styles.sliderStyle}
+            maximumValue={100}
+            minimumValue={0}
+            value={initialvalue}
+            maximumTrackTintColor="lightgray"
+            onSlidingComplete={(val) => {
+              setProgressValue(Math.round(val));
+            }}
+          />
+          <Text style={styles.progressStyle}>{progressValue}%</Text>
+        </View>
+        <Text
+          style={[styles.progressStyle, { alignSelf: 'center', marginTop: 20 }]}
+        >
+          Circle Size
+        </Text>
+        <View style={styles.sliderContainerStyle}>
+          <Slider
+            style={styles.sliderStyle}
+            maximumValue={450}
+            minimumValue={150}
+            value={150}
+            maximumTrackTintColor="lightgray"
+            onSlidingComplete={(val) => {
+              setCircleSize(Math.round(val));
+              setProgressValue(progressValue);
+            }}
+          />
+          <Text style={styles.progressStyle}>{circleSize}</Text>
+        </View>
       </View>
-      <SpeedView percentage={progressValue} enableBounceEffect={false} />
+      <SpeedView
+        percentage={progressValue}
+        size={circleSize}
+        showProgress={true}
+        gradientColorStart={{ color: '#de6262', opacity: '0.6' }}
+        gradientColorEnd={{ color: '#ffb88c', opacity: '0.6' }}
+      />
     </SafeAreaView>
   );
 };
@@ -31,6 +67,7 @@ const styles = StyleSheet.create({
   safeAreaStyle: {
     // backgroundColor: '#444B6F',
     backgroundColor: '#444B6F',
+    flex: 1,
   },
   sliderContainerStyle: {
     flexDirection: 'row',
--- src/SpeedView.tsx
+++ src/SpeedView.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, StyleSheet, Animated, Easing, Dimensions } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, Easing, Dimensions } from 'react-native';
 import Svg, {
   Circle,
   Defs,
   LinearGradient,
-  Stop,
   RadialGradient,
-  Text,
+  Stop,
+  Text as SvgText,
 } from 'react-native-svg';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
@@ -15,7 +15,9 @@ interface GradientColor {
   color: string;
   opacity: number | string;
 }
-interface SpeedViewProps {
+
+interface SpeedCircleViewProps {
+  size: number;
   percentage: number;
   maxPercentage?: number;
   gradientColorStart?: GradientColor;
@@ -25,7 +27,10 @@ interface SpeedViewProps {
   progressFormatSign?: string;
 }
 
-const SpeedView: React.FC<SpeedViewProps> = ({
+const BACKGROUND_STROKE_COLOR = '#303858';
+
+const SpeedView: React.FC<SpeedCircleViewProps> = ({
+  size,
   percentage = 75,
   maxPercentage = 100,
   gradientColorStart = { color: '#00D9F5', opacity: 1.0 },
@@ -34,24 +39,24 @@ const SpeedView: React.FC<SpeedViewProps> = ({
   enableBounceEffect = true,
   progressFormatSign = undefined,
 }) => {
-  /* Circle design layout and circular radius */
-  const BACKGROUND_STROKE_COLOR = '#303858';
-  const { width, height } = Dimensions.get('window');
+  const center = size / 2;
+  const radius = size / 2 - 50 / 2;
+  const innerCircleRadius = radius / 1.55;
 
-  const OUTER_CIRCLE_LENGTH = 1000; // 2PI*R
-  const Outer_R = OUTER_CIRCLE_LENGTH / (2 * Math.PI);
-  const INNER_CIRCLE_LENGTH = 680; // 2PI*R
-  const Inner_R = INNER_CIRCLE_LENGTH / (2 * Math.PI);
-
-  // -----
   const circleRef = useRef<any>();
-  const circleCircumReference = 2 * Math.PI * Inner_R;
+  const circleCircumReference = 2 * Math.PI * innerCircleRadius;
   const animatedValue = useRef(new Animated.Value(0)).current;
   const [circlePercentage, setCerclePercentage] = useState<number>(0);
-  const iTimeInterval: number = 600;
+  const iTimeInterval: number = 500;
   const iTimeStoper: number = 2200;
   const duration: number = 600;
   const delay: number = 0;
+
+  useEffect(() => {
+    if (size > Dimensions.get('screen').width) {
+      throw Error('Circle size should be undder screen width size.');
+    }
+  }, [size]);
 
   useEffect(() => {
     repeateAnimtion();
@@ -76,14 +81,10 @@ const SpeedView: React.FC<SpeedViewProps> = ({
   */
   let timeInterval: NodeJS.Timer;
   const repeateAnimtion = () => {
-    // if (enableBounceEffect) {
     clearInterval(timeInterval);
     if (enableBounceEffect) {
       timeInterval = setInterval(() => {
-        let newPercentage = getRandomNumberFromRange(
-          getRandomNumberFromRange(20, 90),
-          percentage
-        );
+        let newPercentage = randomNumber(randomNumber(20, 90), percentage);
         animation(newPercentage);
         setCerclePercentage(Math.round(newPercentage));
         startAnimation();
@@ -98,15 +99,6 @@ const SpeedView: React.FC<SpeedViewProps> = ({
       },
       enableBounceEffect ? iTimeStoper : iTimeInterval
     );
-    // } else {
-    //   const maxPer = (100 * percentage) / maxPercentage;
-    //   let strokeDashoffset =
-    //     circleCircumReference - (circleCircumReference * maxPer) / 100;
-    //   setCerclePercentage(Math.round(maxPer));
-    //   circleRef?.current?.setNativeProps({
-    //     strokeDashoffset,
-    //   });
-    // }
   };
 
   const startAnimation = () => {
@@ -123,154 +115,107 @@ const SpeedView: React.FC<SpeedViewProps> = ({
     }
   };
 
-  function getRandomNumberFromRange(min: number = 1, max: number = 100) {
+  function randomNumber(min: number = 1, max: number = 100) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
-    <View style={styles.container}>
-      <Svg style={styles.containerSVG}>
-        {/* Most Outer Circle */}
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={Outer_R}
-          stroke={BACKGROUND_STROKE_COLOR}
-          strokeWidth={50}
-        />
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={Outer_R / 1.02}
-          stroke={'url(#outerpath)'}
-          strokeOpacity={1}
-          strokeWidth={15}
-          strokeDasharray={2}
-        />
-        {/* Inner Circle */}
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={Outer_R / 2}
-          fill={BACKGROUND_STROKE_COLOR}
-          strokeWidth={20}
-        />
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={Outer_R / 2.4}
-          stroke={'#00D9F5'}
-          strokeWidth={8}
-          strokeDasharray={1}
-        />
+    <Svg width={size} height={size} style={{ alignSelf: 'center' }}>
+      <Circle
+        cx={center}
+        cy={center}
+        r={radius}
+        stroke={BACKGROUND_STROKE_COLOR}
+        strokeWidth={size / 7}
+      />
+      <Circle
+        cx={center}
+        cy={center}
+        r={radius}
+        stroke={'url(#outerpath)'}
+        strokeOpacity={1}
+        strokeWidth={size / 20}
+        strokeDasharray={2}
+      />
+      {/* Inner Circle */}
+      <Circle
+        cx={center}
+        cy={center}
+        r={radius / 2.1}
+        fill={BACKGROUND_STROKE_COLOR}
+      />
 
-        <Defs>
-          <RadialGradient
-            id="outerpath"
-            cx="300"
-            cy="550"
-            rx="450"
-            ry="450"
-            fx="200"
-            fy="450"
-            gradientUnits="userSpaceOnUse"
-          >
-            <>
-              <Stop offset="0" stopColor="#F9F9F9" stopOpacity={0.5} />
-              <Stop offset="1" stopColor="#01F3A6" stopOpacity={1.0} />
-            </>
-          </RadialGradient>
-        </Defs>
-      </Svg>
+      <Circle
+        cx={center}
+        cy={center}
+        r={radius / 2.5}
+        stroke={'#00D9F5'}
+        strokeWidth={size / 45}
+        strokeDasharray={1}
+      />
 
-      {/* Outer Circle */}
-      <Svg style={styles.grdientSvg}>
-        {/* Gradient animated circle */}
-        <AnimatedCircle
-          ref={circleRef}
-          cx={width / 2}
-          cy={height / 2}
-          r={Inner_R}
-          stroke={'url(#path)'}
-          strokeWidth={15}
-          strokeDasharray={circleCircumReference}
-          strokeDashoffset={circleCircumReference}
-          strokeLinecap={'round'}
-          transform={`rotate(-90, ${width / 2}, ${height / 2})`}
-        />
-        {showProgress && (
+      {showProgress && (
+        <SvgText
+          x={center - 10}
+          y={center + 5}
+          fontSize={size / 15}
+          letterSpacing={10}
+          fontWeight="800"
+          fill="white"
+          textAnchor={'middle'}
+        >
+          {circlePercentage}
+          {progressFormatSign == undefined ? '%' : progressFormatSign}
+        </SvgText>
+      )}
+
+      <Defs>
+        <RadialGradient
+          id="outerpath"
+          cx="300"
+          cy="550"
+          rx="450"
+          ry="450"
+          fx="200"
+          fy="450"
+          gradientUnits="userSpaceOnUse"
+        >
           <>
-            <Text
-              x={width / (progressFormatSign != undefined ? 2 : 2.15)}
-              y={height / (progressFormatSign != undefined ? 2.05 : 1.97)}
-              fontSize={25}
-              fontWeight="800"
-              fill="white"
-              textAnchor={'middle'}
-            >
-              {circlePercentage} {progressFormatSign == undefined && '%'}
-            </Text>
-            {progressFormatSign != undefined && (
-              <Text
-                x={width / 2}
-                y={height / 1.93}
-                fontSize={20}
-                fontWeight="800"
-                fill="white"
-                textAnchor={'middle'}
-              >
-                {progressFormatSign}
-              </Text>
-            )}
+            <Stop offset="0" stopColor="#F9F9F9" stopOpacity={0.5} />
+            <Stop offset="1" stopColor="#01F3A6" stopOpacity={1.0} />
           </>
-        )}
-        <Defs>
-          <LinearGradient id="path" x1={'70%'} y1={'20%'} x2={'40%'} y2={'10%'}>
-            <Stop
-              offset="0"
-              stopColor={gradientColorStart.color}
-              stopOpacity={gradientColorStart.opacity}
-            />
-            <Stop
-              offset="1"
-              stopColor={gradientColorEnd.color}
-              stopOpacity={gradientColorEnd.opacity}
-            />
-          </LinearGradient>
-        </Defs>
-      </Svg>
-    </View>
+        </RadialGradient>
+      </Defs>
+
+      <AnimatedCircle
+        ref={circleRef}
+        cx={center}
+        cy={center}
+        r={innerCircleRadius}
+        stroke={'url(#path)'}
+        strokeWidth={size / 20}
+        strokeDasharray={circleCircumReference}
+        strokeDashoffset={circleCircumReference}
+        strokeLinecap={'round'}
+        transform={`rotate(-90, ${center}, ${center})`}
+      />
+
+      <Defs>
+        <LinearGradient id="path" x1={'70%'} y1={'20%'} x2={'40%'} y2={'10%'}>
+          <Stop
+            offset="0"
+            stopColor={gradientColorStart.color}
+            stopOpacity={gradientColorStart.opacity}
+          />
+          <Stop
+            offset="1"
+            stopColor={gradientColorEnd.color}
+            stopOpacity={gradientColorEnd.opacity}
+          />
+        </LinearGradient>
+      </Defs>
+    </Svg>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: '#444B6F',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  containerSVG: {
-    position: 'absolute',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 9,
-  },
-  grdientSvg: {
-    shadowColor: '#00F5A0',
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 1,
-    shadowRadius: 25,
-    elevation: 9,
-  },
-});
 
 export default SpeedView;
